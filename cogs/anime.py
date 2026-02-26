f4a40c83462a74f2113383d79cbcb0f68dfb3e93
bmisz <benmisner26@gmail.com>
2026-02-26 18:30:51 -0500

    Add database backup functionality and improve change detection logic

--- cogs/anime.py
+++ cogs/anime.py
@@ -1,8 +1,7 @@
-import discord
-import sqlite3
-import datetime
-import time
+from datetime import datetime, time, timezone
 import requests
+import os
+import sqlite3
 from discord.ext import commands, tasks
 from langdetect import detect
 
@@ -20,6 +19,12 @@ class AnimeAnnouncer(commands.Cog):
                 return synonym
         return None     # No synonyms were in english
 
+    @staticmethod
+    def convert_epoch_to_local(unix_epoch_time):
+        dt_unix = datetime.fromtimestamp(unix_epoch_time)
+        formatted_time = dt_unix.strftime("%a %d %b, %I:%M%p")
+        return formatted_time
+    
     @commands.command(name="track")
     async def track(self, ctx, anime_id: str = ""):
         try: 
@@ -182,8 +187,17 @@ class AnimeAnnouncer(commands.Cog):
         response = requests.post(url, json={'query': query, 'variables': variables})
         data = response.json()
 
+        changes = self._look_for_changes(data)
+
+        if changes == False:
+            print(f"Searched through {len(ids)} shows and found no changes.")
+
+    async def _look_for_changes(self, anilist_data):
+        CHANNEL = self.bot.get_channel(self.channel_id)
+        cursor = self.bot.connection.cursor()
         found_change = False
-        for show in data['data']['Page']['media']:
+
+        for show in anilist_data['data']['Page']['media']:
             anilist_english_name = show['title']['english']
             anilist_status = show['status']
             anilist_time = show['nextAiringEpisode']['airingAt'] if show['nextAiringEpisode'] else None
@@ -208,40 +222,41 @@ class AnimeAnnouncer(commands.Cog):
             if anilist_status != db_status and anilist_status == "FINISHED":
                 found_change = True
                 cursor.execute("DELETE FROM tracked_anime WHERE anilist_id = ?", (show['id'],))
+                print(f"Removing {show['id']} from database due to show concluding.")
                 self.bot.connection.commit()
-                await CHANNEL.send(f"❌ **{show['title']['english']}** has concluded and has been removed from your tracking list. ❌")
+                await CHANNEL.send(f"❌ **{db_english_title}** has concluded and has been removed from your tracking list. ❌")
                 continue
 
             if anilist_status == "RELEASING" and db_status == "NOT_YET_RELEASED":
                 found_change = True
                 cursor.execute("UPDATE tracked_anime SET status = ? WHERE anilist_id = ?", (anilist_status, show['id']))
+                print(f"Changing database status: {db_status} -> {anilist_status}. ({show['id']})")
                 self.bot.connection.commit()
-                await CHANNEL.send(f"🚨 **{show['title']['english']}** has started airing! 🚨")
+                await CHANNEL.send(f"🚨 **{db_english_title}** has started airing! 🚨")
             
             elif anilist_status != db_status:
                 found_change = True
                 cursor.execute("UPDATE tracked_anime SET status = ? WHERE anilist_id = ?", (anilist_status, show['id']))
+                print(f"Changing database status: {db_status} -> {anilist_status}. ({show['id']})")
                 self.bot.connection.commit()
-                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{show['title']['english']}**'s status has changed. \n{db_status} ➡️ {anilist_status}")
+                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{db_english_title}**'s status has changed. \n{db_status} ➡️ {anilist_status}")
             
             if anilist_startDate != db_startDate:
                 found_change = True
                 cursor.execute("UPDATE tracked_anime SET startDate = ? WHERE anilist_id = ?", (anilist_startDate, show['id']))
+                print(f"Changing database startDate: {db_startDate} -> {anilist_startDate}. ({show['id']})")
                 self.bot.connection.commit()
-                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{show['title']['english']}**'s start date has changed. \n{db_startDate} ➡️ {anilist_startDate}")
+                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{db_english_title}**'s start date has changed. \n{db_startDate} ➡️ {anilist_startDate}")
 
             if anilist_time != db_time:
                 found_change = True
                 cursor.execute("UPDATE tracked_anime SET next_episode_time = ? WHERE anilist_id = ?", (anilist_time, show['id']))
+                print(f"Changing database time: {db_time} -> {anilist_time}. ({show['id']})")
                 self.bot.connection.commit()
-                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{show['title']['english']}**'s next episode airing time has changed. \n{db_time} ➡️ {anilist_time}")
+                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{db_english_title}**'s next episode airing time has changed. \n{db_time} ➡️ {self.convert_epoch_to_local(anilist_time)}")
+        return found_change
 
-        if not found_change:
-            print(f"Checked {len(ids)} shows. No changes found.")
-
-
-    time = datetime.time(hour=21, tzinfo=datetime.UTC)
-    @tasks.loop(time=time)
+    @tasks.loop(time=time(hour=21, tzinfo=timezone.utc))
     async def week_reminder(self):
         CHANNEL = self.bot.get_channel(self.channel_id)
         if CHANNEL is None:
@@ -250,7 +265,7 @@ class AnimeAnnouncer(commands.Cog):
         # One week in seconds
         ONE_WEEK = 60 * 60 * 24 * 7 
 
-        NOW = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+        NOW = int(datetime.now(timezone.utc).timestamp())
         
         cursor = self.bot.connection.cursor()
         cursor.execute("SELECT anilist_id, title_english, next_episode_time, weekly_reminder_sent FROM tracked_anime")
@@ -276,7 +291,27 @@ class AnimeAnnouncer(commands.Cog):
                 self.bot.connection.commit()
                 await CHANNEL.send(f"🔔 **{english_title}** is less than one week from airing it's first episode! 🔔")
             
+    @tasks.loop(hours=72)
+    async def database_backup(self):
+        if not os.path.exists('backups'):
+            os.makedirs('backups')
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_file = f"backups/database_backup_{timestamp}.db"
+
+        try:
+            source = self.bot.connection
+            dest = sqlite3.connect(backup_file)
+
+            with dest:
+                source.backup(dest)
             
+            print(f"✅ Backup successful: {backup_file}")
+        except Exception as e:
+            print(f"❌ Backup failed: {e}")
+        finally:
+            dest.close()
+
     @query_anilist.before_loop
     async def before_query_anilist(self):
         print('Waiting until bot is initilized until querying AniList...')
