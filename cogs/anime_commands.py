92ec2113538c459684d8ae2d3cfb8effc58b8659
bmisz <benmisner26@gmail.com>
2026-03-04 18:48:24 -0500

    Refactor code formatting and improve readability in anime_commands.py and anime_tasks.py

--- cogs/anime_commands.py
+++ cogs/anime_commands.py
@@ -4,13 +4,14 @@ from discord.ext import commands
 from langdetect import detect
 from .util_methods import convert_epoch_to_local, determine_english_title
 
+
 class AnimeAnnouncerCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="info")
     async def info(self, ctx, anime_id: str = ""):
-        try: 
+        try:
             anime_id_int = int(anime_id)
         except ValueError:
             await ctx.send("This is not a valid integer ID")
@@ -19,22 +20,33 @@ class AnimeAnnouncerCommands(commands.Cog):
             await ctx.send("Please enter an ID you track to get info on it")
             return
         cursor = self.bot.connection.cursor()
-        cursor.execute("SELECT * FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,))
+        cursor.execute(
+            "SELECT * FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,)
+        )
         row = cursor.fetchone()
         if row:
-            anilist_id, title_english, title_romaji, next_episode_time, start_date, status, weekly_reminder_sent = row
-            final_message = (f"# **{title_english}** *{anilist_id}*\n"
-            f"**Romaji Title:** {title_romaji}\n"
-            f"**Status:** {status}\n"
-            f"**Start date:** {start_date}\n"
-            f"**Next episode airs:** {convert_epoch_to_local(next_episode_time)}\n"
-            f"**Weekly reminder sent?:** {"Yes" if weekly_reminder_sent == 1 else "No"}"
+            (
+                anilist_id,
+                title_english,
+                title_romaji,
+                next_episode_time,
+                start_date,
+                status,
+                weekly_reminder_sent,
+            ) = row
+            final_message = (
+                f"# **{title_english}** *{anilist_id}*\n"
+                f"**Romaji Title:** {title_romaji}\n"
+                f"**Status:** {status}\n"
+                f"**Start date:** {start_date}\n"
+                f"**Next episode airs:** {convert_epoch_to_local(next_episode_time)}\n"
+                f"**Weekly reminder sent?:** {"Yes" if weekly_reminder_sent == 1 else "No"}"
             )
             await ctx.send(final_message)
 
     @commands.command(name="track")
     async def track(self, ctx, anime_id: str = ""):
-        try: 
+        try:
             anime_id_int = int(anime_id)
         except ValueError:
             await ctx.send("This is not a valid integer ID")
@@ -42,7 +54,7 @@ class AnimeAnnouncerCommands(commands.Cog):
         if anime_id == "":
             await ctx.send("Please enter an ID to start tracking.")
         else:
-            query = '''
+            query = """
             query ($id: Int) {
                 Media (id: $id, type: ANIME) {
                     id
@@ -60,24 +72,28 @@ class AnimeAnnouncerCommands(commands.Cog):
                     synonyms
                 }
             }
-            '''
+            """
 
-            variables = {
-                'id': anime_id_int
-            }
-            url = 'https://graphql.anilist.co'
+            variables = {"id": anime_id_int}
+            url = "https://graphql.anilist.co"
 
-            response = requests.post(url, json={'query': query, 'variables': variables})
+            response = requests.post(url, json={"query": query, "variables": variables})
             data = response.json()
-            anime = data['data']['Media']
+            anime = data["data"]["Media"]
 
-            if anime['status'] == "FINISHED":
-                await ctx.send("Show is finished and therefore has no reason to be tracked. Not added. ")
+            if anime["status"] == "FINISHED":
+                await ctx.send(
+                    "Show is finished and therefore has no reason to be tracked. Not added. "
+                )
                 return
-            
-            start_date = f"{anime['startDate']['year']}-{anime['startDate']['month']}-{anime['startDate']['day']}"
 
-            next_ep = anime['nextAiringEpisode']['airingAt'] if anime['nextAiringEpisode'] else None
+            start_date = f"{anime["startDate"]["year"]}-{anime["startDate"]["month"]}-{anime["startDate"]["day"]}"
+
+            next_ep = (
+                anime["nextAiringEpisode"]["airingAt"]
+                if anime["nextAiringEpisode"]
+                else None
+            )
 
             sql = """
                 INSERT OR REPLACE INTO tracked_anime 
@@ -86,28 +102,31 @@ class AnimeAnnouncerCommands(commands.Cog):
             """
 
             cursor = self.bot.connection.cursor()
-            
-            cursor.execute("SELECT 1 FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,))
+
+            cursor.execute(
+                "SELECT 1 FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,)
+            )
             if cursor.fetchone():
                 await ctx.send("You've already started tracking this show.")
                 return
 
-            if anime['title']['english'] is None:
-                english_title = determine_english_title(anime['synonyms'])
+            if anime["title"]["english"] is None:
+                english_title = determine_english_title(anime["synonyms"])
                 if english_title is None:
-                    english_title = anime['title']['romaji']   
+                    english_title = anime["title"]["romaji"]
             else:
-                english_title = anime['title']['english']
+                english_title = anime["title"]["english"]
 
             cursor.execute(
-                sql, (
-                    anime['id'], 
-                    english_title, 
-                    anime['title']['romaji'], 
-                    next_ep, 
-                    start_date, 
-                    anime['status']
-                )
+                sql,
+                (
+                    anime["id"],
+                    english_title,
+                    anime["title"]["romaji"],
+                    next_ep,
+                    start_date,
+                    anime["status"],
+                ),
             )
             self.bot.connection.commit()
 
@@ -123,12 +142,17 @@ class AnimeAnnouncerCommands(commands.Cog):
         if anime_id == "":
             await ctx.send("Please enter an ID to stop tracking.")
             return
-        
+
         cursor = self.bot.connection.cursor()
-        cursor.execute("SELECT title_english FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,))
+        cursor.execute(
+            "SELECT title_english FROM tracked_anime WHERE anilist_id = ?",
+            (anime_id_int,),
+        )
         name = cursor.fetchone()
         name = name[0]
-        cursor.execute("DELETE FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,))
+        cursor.execute(
+            "DELETE FROM tracked_anime WHERE anilist_id = ?", (anime_id_int,)
+        )
         self.bot.connection.commit()
         await ctx.send(f"Stopped tracking **{name} ({anime_id_int})**")
 
@@ -138,16 +162,19 @@ class AnimeAnnouncerCommands(commands.Cog):
         cursor.execute("SELECT anilist_id, title_english FROM tracked_anime")
         rows = cursor.fetchall()
         if not rows:
-            await ctx.send("**You're not currently tracking any animes, to get started use !track *id.***")
+            await ctx.send(
+                "**You're not currently tracking any animes, to get started use !track *id.***"
+            )
             return
         rows_as_strings = []
         for id, title in rows:
             rows_as_strings.append(f"**• {title}** *(ID: {id})*")
-        
+
         rows_as_strings.insert(0, "# Tracked Animes:")
         final_string = "\n".join(rows_as_strings)
-        
+
         await ctx.send(final_string)
+
 
 async def setup(bot):
     await bot.add_cog(AnimeAnnouncerCommands(bot))
--- cogs/anime_tasks.py
+++ cogs/anime_tasks.py
@@ -5,6 +5,7 @@ from datetime import datetime, time, timezone
 import sqlite3
 from .util_methods import convert_epoch_to_local
 
+
 class AnimeAnnouncerTasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,13 +13,13 @@ class AnimeAnnouncerTasks(commands.Cog):
         self.query_anilist.start()
         self.week_reminder.start()
         self.database_backup.start()
-    
+
     @tasks.loop(minutes=30)
     async def query_anilist(self):
         CHANNEL = self.bot.get_channel(self.channel_id)
         if CHANNEL is None:
             CHANNEL = await self.bot.fetch_channel(self.channel_id)
-        
+
         cursor = self.bot.connection.cursor()
         cursor.execute("SELECT anilist_id FROM tracked_anime")
 
@@ -28,8 +29,8 @@ class AnimeAnnouncerTasks(commands.Cog):
         if not ids:
             print("Not tracking any anime yet, skipping any checking.")
             return
-        
-        query = '''
+
+        query = """
         query ($ids: [Int]) {
             Page {
                 media (id_in: $ids, type: ANIME) {
@@ -48,14 +49,12 @@ class AnimeAnnouncerTasks(commands.Cog):
                 }
             }
         }
-        '''
+        """
 
-        variables = {
-            'ids': ids
-        }
-        url = 'https://graphql.anilist.co'
+        variables = {"ids": ids}
+        url = "https://graphql.anilist.co"
 
-        response = requests.post(url, json={'query': query, 'variables': variables})
+        response = requests.post(url, json={"query": query, "variables": variables})
         data = response.json()
 
         changes = await self._look_for_changes(data)
@@ -70,63 +69,89 @@ class AnimeAnnouncerTasks(commands.Cog):
         print("Looking for changes...")
         found_change = False
 
-        for show in anilist_data['data']['Page']['media']:
-            anilist_english_name = show['title']['english']
-            anilist_status = show['status']
-            anilist_time = show['nextAiringEpisode']['airingAt'] if show['nextAiringEpisode'] else None
-            startDate_as_unix_epoch = show['startDate']
-            anilist_startDate = f"{startDate_as_unix_epoch['year']}-{startDate_as_unix_epoch['month']}-{startDate_as_unix_epoch['day']}"
-  
+        for show in anilist_data["data"]["Page"]["media"]:
+            anilist_english_title = show["title"]["english"]
+            anilist_status = show["status"]
+            anilist_next_airing_episode = (
+                show["nextAiringEpisode"]["airingAt"]
+                if show["nextAiringEpisode"]
+                else None
+            )
+            startDate = show["startDate"]
+            anilist_startDate = (
+                f"{startDate['year']}-{startDate['month']}-{startDate['day']}"
+            )
 
-            cursor.execute("SELECT status, next_episode_time, startDate, title_english FROM tracked_anime WHERE anilist_id = ?", (show['id'],))
+            cursor.execute(
+                "SELECT status, next_episode_time, startDate, title_english FROM tracked_anime WHERE anilist_id = ?",
+                (show["id"],),
+            )
             row = cursor.fetchone()
-            if row:
-                db_status = row[0]
-                db_time = row[1]
-                db_startDate = row[2]
-                db_english_title = row[3]
+            if not row:
+                continue
 
-            
-            if (anilist_english_name != db_english_title) and (anilist_english_name is not None):
-                print(f"English title has changed from {db_english_title} to {anilist_english_name}")
-                found_change = True
-                cursor.execute("UPDATE tracked_anime SET title_english = ? WHERE anilist_id = ?", (anilist_english_name, show['id']))
-            
+            db_status, db_next_airing_episode, db_startDate, db_english_title = row
+
             if anilist_status != db_status and anilist_status == "FINISHED":
                 found_change = True
-                cursor.execute("DELETE FROM tracked_anime WHERE anilist_id = ?", (show['id'],))
+                cursor.execute(
+                    "DELETE FROM tracked_anime WHERE anilist_id = ?", (show["id"],)
+                )
                 print(f"Removing {show['id']} from database due to show concluding.")
                 self.bot.connection.commit()
-                await CHANNEL.send(f"❌ **{db_english_title}** has concluded and has been removed from your tracking list. ❌")
+                await CHANNEL.send(
+                    f"❌ **{db_english_title}** has concluded and has been removed from your tracking list. ❌"
+                )
                 continue
 
             if anilist_status == "RELEASING" and db_status == "NOT_YET_RELEASED":
                 found_change = True
-                cursor.execute("UPDATE tracked_anime SET status = ? WHERE anilist_id = ?", (anilist_status, show['id']))
-                print(f"Changing database status: {db_status} -> {anilist_status}. ({show['id']})")
+                cursor.execute(
+                    "UPDATE tracked_anime SET status = ? WHERE anilist_id = ?",
+                    (anilist_status, show["id"]),
+                )
+                print(
+                    f"Changing database status: {db_status} -> {anilist_status}. ({show['id']})"
+                )
                 self.bot.connection.commit()
+                db_status = "RELEASING"
                 await CHANNEL.send(f"🚨 **{db_english_title}** has started airing! 🚨")
-            
-            elif anilist_status != db_status:
-                found_change = True
-                cursor.execute("UPDATE tracked_anime SET status = ? WHERE anilist_id = ?", (anilist_status, show['id']))
-                print(f"Changing database status: {db_status} -> {anilist_status}. ({show['id']})")
-                self.bot.connection.commit()
-                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{db_english_title}**'s status has changed. \n{db_status} ➡️ {anilist_status}")
-            
-            if anilist_startDate != db_startDate:
-                found_change = True
-                cursor.execute("UPDATE tracked_anime SET startDate = ? WHERE anilist_id = ?", (anilist_startDate, show['id']))
-                print(f"Changing database startDate: {db_startDate} -> {anilist_startDate}. ({show['id']})")
-                self.bot.connection.commit()
-                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{db_english_title}**'s start date has changed. \n{db_startDate} ➡️ {anilist_startDate}")
 
-            elif anilist_time != db_time:
-                found_change = True
-                cursor.execute("UPDATE tracked_anime SET next_episode_time = ? WHERE anilist_id = ?", (anilist_time, show['id']))
-                print(f"Changing database time: {db_time} -> {anilist_time}. ({show['id']})")
-                self.bot.connection.commit()
-                await CHANNEL.send(f"⚠️ UPDATE ⚠️: **{db_english_title}**'s next episode airing time has changed. \n{db_time} ➡️ {convert_epoch_to_local(anilist_time)}")
+            chnages_to_look_for = [
+                # anilist_info, db_info, db_column name, display_name
+                (
+                    anilist_english_title,
+                    db_english_title,
+                    "title_english",
+                    "english title",
+                ),
+                (anilist_status, db_status, "status", "status"),
+                (
+                    anilist_next_airing_episode,
+                    db_next_airing_episode,
+                    "next_episode_time",
+                    "next episode airing time",
+                ),
+                (anilist_startDate, db_startDate, "startDate", "start date"),
+            ]
+            for new_val, old_val, db_column, label in chnages_to_look_for:
+                if new_val is not None and new_val != old_val:
+                    cursor.execute(
+                        f"UPDATE tracked_anime SET {db_column} = ? WHERE anilist_id = ?",
+                        (new_val, show["id"]),
+                    )
+                    print(f"{label} changed: {old_val} --> {new_val}.")
+
+                    if "next_episode_time" in db_column:
+                        new_val = convert_epoch_to_local(new_val)
+                    await CHANNEL.send(
+                        f"⚠️ UPDATE ⚠️: **{db_english_title}**'s {label} has changed. \n**{old_val} ➡️ {new_val}**"
+                    )
+
+                    found_change = True
+        if found_change:
+            self.bot.connection.commit()
+            
         return found_change
 
     @tasks.loop(time=time(hour=21, tzinfo=timezone.utc))
@@ -134,21 +159,23 @@ class AnimeAnnouncerTasks(commands.Cog):
         CHANNEL = self.bot.get_channel(self.channel_id)
         if CHANNEL is None:
             CHANNEL = await self.bot.fetch_channel(self.channel_id)
-    
+
         # One week in seconds
-        ONE_WEEK = 60 * 60 * 24 * 7 
+        ONE_WEEK = 60 * 60 * 24 * 7
 
         NOW = int(datetime.now(timezone.utc).timestamp())
-        
+
         cursor = self.bot.connection.cursor()
-        cursor.execute("SELECT anilist_id, title_english, next_episode_time, weekly_reminder_sent FROM tracked_anime")
+        cursor.execute(
+            "SELECT anilist_id, title_english, next_episode_time, weekly_reminder_sent FROM tracked_anime"
+        )
 
         rows = cursor.fetchall()
 
-        if not rows: 
+        if not rows:
             print("Not tracking any anime yet, skipping checking for weekly reminder.")
             return
-        
+
         for id, english_title, next_episode_time, weekly_reminder_already_sent in rows:
             weekly_reminder_already_sent = bool(weekly_reminder_already_sent)
             if weekly_reminder_already_sent == True:
@@ -160,14 +187,19 @@ class AnimeAnnouncerTasks(commands.Cog):
 
             time_to_next_ep = next_episode_time - NOW
             if 0 < time_to_next_ep <= ONE_WEEK:
-                cursor.execute("UPDATE tracked_anime SET weekly_reminder_sent = 1 WHERE anilist_id = ?", (id,))
+                cursor.execute(
+                    "UPDATE tracked_anime SET weekly_reminder_sent = 1 WHERE anilist_id = ?",
+                    (id,),
+                )
                 self.bot.connection.commit()
-                await CHANNEL.send(f"🔔 **{english_title}** is less than one week from airing it's first episode! 🔔")
-            
+                await CHANNEL.send(
+                    f"🔔 **{english_title}** is less than one week from airing it's first episode! 🔔"
+                )
+
     @tasks.loop(hours=72)
     async def database_backup(self):
-        if not os.path.exists('backups'):
-            os.makedirs('backups')
+        if not os.path.exists("backups"):
+            os.makedirs("backups")
 
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         backup_file = f"backups/database_backup_{timestamp}.db"
@@ -178,7 +210,7 @@ class AnimeAnnouncerTasks(commands.Cog):
 
             with dest:
                 source.backup(dest)
-            
+
             print(f"Backup successful: {backup_file}")
         except Exception as e:
             print(f"Backup failed: {e}")
@@ -187,18 +219,23 @@ class AnimeAnnouncerTasks(commands.Cog):
 
     @query_anilist.before_loop
     async def before_query_anilist(self):
-        print('Waiting until bot is initialized until querying AniList...')
+        print("Waiting until bot is initialized until querying AniList...")
         await self.bot.wait_until_ready()
 
     @week_reminder.before_loop
     async def before_week_reminder(self):
-        print('Waiting until bot is initialized before starting weekly reminders task...')
+        print(
+            "Waiting until bot is initialized before starting weekly reminders task..."
+        )
         await self.bot.wait_until_ready()
-    
+
     @database_backup.before_loop
     async def before_database_backup(self):
-        print('Waiting until bot is initialized before starting database backup task...')
+        print(
+            "Waiting until bot is initialized before starting database backup task..."
+        )
         await self.bot.wait_until_ready()
+
 
 async def setup(bot):
     await bot.add_cog(AnimeAnnouncerTasks(bot))
