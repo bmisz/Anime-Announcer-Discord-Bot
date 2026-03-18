900d3a1e0539dc451f2450a67047379398e29c22
bmisz <benmisner26@gmail.com>
2026-03-18 18:08:19 -0400

    fix: changed shutdown command's name  to 'shutdown'

--- cogs/general.py
+++ cogs/general.py
@@ -6,11 +6,9 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # A simple "ping" command to check if the bot is alive
     @commands.command(name="ping")
     async def ping(self, ctx):
-        """Checks the bot's latency."""
-        await ctx.send(f"🏓 Pong! {round(self.bot.latency * 1000)}ms")
+        await ctx.send(f"{round(self.bot.latency * 1000)}ms")
 
     @commands.command()
     async def checkid(self, ctx):
@@ -18,23 +16,14 @@ class General(commands.Cog):
         await ctx.send(f"Your Discord ID is: {user_id}")
 
     @commands.command()
-    async def killyourself(self, ctx):
+    async def shutdown(self, ctx):
         await ctx.send("Shutting down...")
         await self.bot.close()
 
-    # A command that repeats what you say
     @commands.command(name="echo")
     async def echo(self, ctx, *, message: str):
-        """Repeats your message."""
         await ctx.send(message)
 
-    # A "meaningless" foo command that takes an argument
-    @commands.command(name="foo")
-    async def foo(self, ctx, bar: str = "Nothing"):
-        """Just a foo command."""
-        await ctx.send(f"You provided: {bar}")
 
-
-# This function is required for the bot to load this file
 async def setup(bot):
     await bot.add_cog(General(bot))
